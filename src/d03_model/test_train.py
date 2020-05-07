d19ac7e368c9a791036a528c2bb1a4fe44577861
JishnuJayaraj <jishnujayaraj@live.com>
2020-05-08 00:45:15 +0200

    restructured the project

---
+++ src/d03_model/test_train.py
@@ -0,0 +1,11 @@
+#from d06_visualization.visualize import plot
+
+from d06_visualization.visualize import plot
+# kmeans/ clustering
+def sample(x,y):
+    plot(x,y,'bx','x albel','y label','My title','image.png')
+
+
+
+
+
